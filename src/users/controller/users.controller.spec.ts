e90ef8fd3fbda9dc0d82e67ee6ef72f6ca990179
Yaroslav Malykhin <yaroslav.malykhin@aimprosoft.com>
2022-10-22 10:43:58 +0300

    fix(users): Fixed users tests

--- src/users/controller/users.controller.spec.ts
+++ src/users/controller/users.controller.spec.ts
@@ -1,14 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from '../service/users.service';
+import { UserEntity } from '../entity/user.entity';
+import { Roles } from '../../utils';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  const userEntityMock: UserEntity = {
+    id: Number(new Date()),
+    telegramId: Number(new Date()),
+    firstName: 'firstName',
+    lastName: 'lastName',
+    role: Roles.CLIENT,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: userEntityMock,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
--- src/users/service/users.service.spec.ts
+++ src/users/service/users.service.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
+import { UserEntity } from '../entity/user.entity';
+import { Roles } from '../../utils';
 
 describe('UsersService', () => {
   let service: UsersService;
+  const userEntityMock: UserEntity = {
+    id: Number(new Date()),
+    telegramId: Number(new Date()),
+    firstName: 'firstName',
+    lastName: 'lastName',
+    role: Roles.CLIENT,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: userEntityMock,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
